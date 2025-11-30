114f1c5cf2ca633c8e6b499e92e0d8eab9a7b789
alessandrobare0111-prog <alessandrobare0111@gmail.com>
2025-11-30 17:49:30 +0100

    Update Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -4,11 +4,12 @@ function Sidebar() {
       <h3>VineTrack</h3>
 
       <nav>
-        <a href="#" className="active">Dashboard</a>
-        <a href="#">Vasche</a>
-        <a href="#">Lotti</a>
-        <a href="#">Analisi</a>
-        <a href="#">Imbottigliamenti</a>
+  <a href="#" className="active">Dashboard</a>
+  <a href="#">Vasche</a>
+  <a href="#">Lotti</a>
+  <a href="#">Analisi</a>
+  <a href="#">Imbottigliamenti</a>
+</nav>
       </nav>
     </aside>
   );
