e8ff6c4bb6f79a1ca2a574b4f884c718febdb1e0
alessandrobare0111-prog <alessandrobare0111@gmail.com>
2025-11-30 17:46:51 +0100

    Create Vasche.js

---
+++ client/src/pages/Vasche.js
@@ -0,0 +1,30 @@
+import VascaCard from "../components/VascaCard";
+
+function Vasche() {
+  return (
+    <div className="vasche-page">
+      <h2>Vasche</h2>
+      <p>Situazione attuale delle vasche in cantina</p>
+
+      <div className="vasche-list">
+        <VascaCard
+          id="001"
+          tipo="Acciaio"
+          stato="Fermentazione"
+          volume={1200}
+          capacita={2000}
+        />
+
+        <VascaCard
+          id="002"
+          tipo="Legno"
+          stato="Affinamento"
+          volume={450}
+          capacita={600}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Vasche;
