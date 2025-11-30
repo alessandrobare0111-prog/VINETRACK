6db3d2c4eace5d4d4de8f13c026f0e51ebdf0c1a
alessandrobare0111-prog <alessandrobare0111@gmail.com>
2025-11-30 17:38:57 +0100

    Create Topbar.js

---
+++ client/src/components/Topbar.js
@@ -0,0 +1,10 @@
+function TopBar() {
+  return (
+    <header className="topbar">
+      <h1>VineTrack</h1>
+      <p>Gestione completa della cantina – dal vigneto alla bottiglia</p>
+    </header>
+  );
+}
+
+export default TopBar;
