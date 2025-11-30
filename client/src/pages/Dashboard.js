0a385fdb48184a0c6a4c300785fb03a17b985a06
alessandrobare0111-prog <alessandrobare0111@gmail.com>
2025-11-30 17:37:10 +0100

    Create Dashboard.js

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <p>Benvenuto in VineTrack. Qui vedrai un riepilogo della cantina.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
