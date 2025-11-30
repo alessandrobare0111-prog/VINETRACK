2f31640530b4ec95249736db62ac1d36777deaee
alessandrobare0111-prog <alessandrobare0111@gmail.com>
2025-11-30 17:38:09 +0100

    Create Sidebar.js

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,17 @@
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h3>VineTrack</h3>
+
+      <nav>
+        <a href="#" className="active">Dashboard</a>
+        <a href="#">Vasche</a>
+        <a href="#">Lotti</a>
+        <a href="#">Analisi</a>
+        <a href="#">Imbottigliamenti</a>
+      </nav>
+    </aside>
+  );
+}
+
+export default Sidebar;
