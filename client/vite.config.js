a88a04a6b86ad69ff86d0decd73b6722ef3a1021
alessandrobare0111-prog <alessandrobare0111@gmail.com>
2025-12-03 18:45:59 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  base: "/VINETRACK/", // <-- OBBLIGATORIO per GitHub Pages
+  build: {
+    outDir: "../docs", // <-- la cartella pubblicata da GitHub Pages
+    emptyOutDir: true,
+  },
 });
