9326df8b310a72fde74c227f0eb72ed0cf62a18e
alessandrobare0111-prog <alessandrobare0111@gmail.com>
2025-12-10 21:55:02 +0100

    Create vasche.js

---
+++ client/src/mock/vasche.js
@@ -0,0 +1,30 @@
+// /src/mock/vasche.js
+
+const vasche = [
+  {
+    id: 1,
+    nome: "Vasca Acciaio 01",
+    capacita: 5000,
+    contenuto: 3200,
+    stato: "fermentazione",
+    temperatura: 18,
+  },
+  {
+    id: 2,
+    nome: "Vasca Cemento 03",
+    capacita: 7000,
+    contenuto: 7000,
+    stato: "maturazione",
+    temperatura: 14,
+  },
+  {
+    id: 3,
+    nome: "Vasca Legno 02",
+    capacita: 4000,
+    contenuto: 500,
+    stato: "vuota",
+    temperatura: 12,
+  }
+];
+
+export default vasche;
